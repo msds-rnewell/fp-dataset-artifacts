e754153ed3f9dffc557aefced46a5035db22ebf3
msds-rnewell <112514657+msds-rnewell@users.noreply.github.com>
2023-12-03 18:06:59 -0500

    Update run_new.py

--- run_new.py
+++ run_new.py
@@ -163,7 +163,7 @@ def main():
         )
 
     # Select the training configuration
-    trainer_class = TrainerNew()
+    trainer_class = TrainerNew
     eval_kwargs = {}
     # If you want to use custom metrics, you should define your own "compute_metrics" function.
     # For an example of a valid compute_metrics function, see compute_accuracy in helpers.py.
